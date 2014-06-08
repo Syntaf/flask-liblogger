938a96264a8aab957aa9a507f275198e712dc7b0
Grant Mercer <Gmercer015@gmail.com>
2014-06-08 12:56:35 -0500

    added base html template, more functionality

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = { 'nickname': 'Grant' } # fake user
+	posts = [ # fake array of posts
+		{
+			'author': { 'nickname': 'John'},
+			'body': 'Lovely day in Baton Rouge!'
+		},
+		{
+			'author': { 'nickname': 'Susan'},
+			'body': 'Loved the movie!'
+		}
+	]
+	
+	return render_template("index.html",
+			title = 'Home',
+			user = user,
+			posts = posts)
