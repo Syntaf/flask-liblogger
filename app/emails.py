57b08c26330e31f2b26f4331a24f868008d4eea4
Grant Mercer <Gmercer015@gmail.com>
2014-06-20 11:17:03 -0500

    fixed config imports

--- app/emails.py
+++ app/emails.py
@@ -2,14 +2,14 @@ from flask import render_template
 from flask.ext.mail import Message
 from app import mail
 from decorators import async
-from config import LOGIN, PASSWORD
+from config import KEY, SANDBOX
 from config import ADMINS
 import requests
 
 @async
 def send_email(subject, sender, recipients, text_body, html_body):
-    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
-    request = requests.post(request_url, auth=('api', key), data={
+    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(SANDBOX)
+    request = requests.post(request_url, auth=('api', KEY), data={
         'from': 'lith@example.com',
         'to': ADMINS[0],
         'subject': subject,
