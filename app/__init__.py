9b11fc1684b03cea150e1e776c652a4ad548f3d6
Grant Mercer <Gmercer015@gmail.com>
2014-06-20 10:56:06 -0500

    removed old smtp client method

--- app/__init__.py
+++ app/__init__.py
@@ -19,16 +19,6 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 mail = Mail(app)
 babel = Babel(app)
 
-if not app.debug:
-    import logging
-    from logging.handlers import SMTPHandler
-    credentials = None
-    if MAIL_USERNAME or MAIL_PASSWORD:
-        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'liblogger failure', credentials)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
-
 app.jinja_env.globals['momentjs'] = momentjs
 
 from app import views, models
