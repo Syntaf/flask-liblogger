74c198b53edfc0f8259ff3e5ea1d108975a374e8
Grant Mercer <Gmercer015@gmail.com>
2014-06-21 17:46:50 -0500

    removed email funcitonality

--- app/emails.py
+++ app/emails.py
@@ -7,14 +7,7 @@ from config import ADMINS
 import requests
 
 def send_email(subject, sender, recipients, text_body, html_body):
-    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(SANDBOX)
-    request = requests.post(request_url, auth=('api', KEY), data={
-        'from': 'lith@example.com',
-        'to': ADMINS[0],
-        'subject': subject,
-        'text': text_body
-    })
-
+	return True
     
 def follower_notification(followed, follower):
     send_email("[liblogger] %s is now following you!" % follower.nickname,
