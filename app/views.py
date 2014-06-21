2c5e1d8a511179f460bfd58ce3a5b5f5e10184cd
Grant Mercer <Gmercer015@gmail.com>
2014-06-21 18:26:27 -0500

    OAuth version 2.0 changed

--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ auth_uri = 'https://accounts.google.com/o/oauth2/auth'
 token_uri = 'https://accounts.google.com/o/oauth2/token'
 scope = ('https://www.googleapis.com/auth/userinfo.profile',
          'https://www.googleapis.com/auth/userinfo.email')
-profile_uri = 'https://www.googleapis.com/oauth2/v1/userinfo'
+profile_uri = 'https://www.googleapis.com/oauth2/v2/userinfo'
 
 @lm.user_loader
 def load_user(id):
